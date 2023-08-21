d0420603413b585a5a83c9fbe7c29f79043469db
Hoon <hyo04044@naver.com>
2023-08-21 20:02:31 +0900

    Initial commit

---
+++ src/main/java/loveEncyclopedia/LoveEncyclopedia/LoveEncyclopediaApplication.java
@@ -0,0 +1,13 @@
+package loveEncyclopedia.LoveEncyclopedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveEncyclopediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoveEncyclopediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/loveEncyclopedia/LoveEncyclopedia/LoveEncyclopediaApplicationTests.java
@@ -0,0 +1,13 @@
+package loveEncyclopedia.LoveEncyclopedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveEncyclopediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
