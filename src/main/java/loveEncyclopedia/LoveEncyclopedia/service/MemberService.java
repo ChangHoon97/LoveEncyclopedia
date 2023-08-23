3d2c94720eefafbfbf190607af42bf6ea309a51c
ChangHoon <hyo04044@naver.com>
2023-08-23 19:26:44 +0900

    create:Test Code

---
+++ src/main/java/loveEncyclopedia/LoveEncyclopedia/service/MemberService.java
@@ -0,0 +1,41 @@
+package loveEncyclopedia.LoveEncyclopedia.service;
+
+import loveEncyclopedia.LoveEncyclopedia.domain.Member;
+import loveEncyclopedia.LoveEncyclopedia.repository.MemberRepository;
+import loveEncyclopedia.LoveEncyclopedia.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Long join(Member member){
+        //같은 이름이 있는 중복 회원X
+        ValidateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void ValidateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m->{
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    //전체 회원 조회
+    public List<Member> findMember(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
